3f651cf6389e8e5de5f36ae29cb8a7bcf851f04f
DevSec <johann@objekt.stream>
2021-01-03 19:32:42 +0100

    + memory offsets

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { attach, DataTypes } from './modules/debugger'
+import { attach } from './modules/debugger'
 
 async function main () {
   const process = await attach('Tap Dungeon.exe')
-
-  const moneyAddress = await process.address(process.processName, 0x2F0DD8, 0x24, 0x8)
-  const [money] = await process.memory(moneyAddress)
+  const [money, moneyAddress] = await process.memory(process.processName, 0x2F0DD8, 0x24, 0x8)
 
   for (let index = 0; index < 1000; index++) {
     console.time('write')
@@ -15,6 +13,8 @@ async function main () {
     await money.double()
     console.timeEnd('read')
   }
+
+  console.log(await money.double(), moneyAddress)
 
   process.detach()
 }
