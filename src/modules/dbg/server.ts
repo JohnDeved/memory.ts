ef7e09b42cc7df0632cb1439f7117e7f78b204b4
DevSec <johann@objekt.stream>
2021-01-06 16:44:59 +0100

    + cleanup dbg (closes #9, closes #3)

---
+++ src/modules/dbg/server.ts
@@ -0,0 +1,34 @@
+import { ChildProcessWithoutNullStreams } from 'child_process'
+import { writeFileSync } from 'fs-extra'
+import { workerData as processName, parentPort } from 'worker_threads'
+import { getOutPath, initDbg, sendCommand } from '.'
+
+interface IMessageDate {
+  command: string
+  expect?: string[]
+  collect?: true
+  sync?: true
+  dbg: ChildProcessWithoutNullStreams
+}
+
+init(processName).then(dbg => {
+  parentPort?.on('message', data => handleMessage({ ...data, dbg }))
+})
+
+async function init (processName: string) {
+  const { b64, dbg } = await initDbg(processName, true)
+
+  parentPort?.postMessage({ event: 'init', data: { b64, pid: dbg.pid } })
+
+  return dbg
+}
+
+async function handleMessage ({ dbg, command, expect, collect, sync }: IMessageDate) {
+  const respoonse = await sendCommand(dbg, command, expect, collect)
+
+  if (sync) {
+    return writeFileSync(getOutPath(dbg.pid), respoonse)
+  }
+
+  parentPort?.postMessage({ event: command, data: respoonse })
+}
