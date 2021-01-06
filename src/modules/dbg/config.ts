745edbd63022772bf724af24f70e81bc24097f74
DevSec <johann@objekt.stream>
2021-01-07 00:39:59 +0100

    + use os tmp folder for io file

--- src/modules/dbg/config.ts
+++ src/modules/dbg/config.ts
@@ -1,8 +1,9 @@
 import path from 'path'
+import os from 'os'
 
 export const binDir = path.resolve(__dirname, '..', '..', '..', 'bin')
 export const cdb32 = path.resolve(binDir, 'cdb32.exe')
 export const cdb64 = path.resolve(binDir, 'cdb64.exe')
 export const tlist = path.resolve(binDir, 'tlist.exe')
-export const out = path.resolve(__dirname, 'out')
+export const out = path.resolve(os.tmpdir(), 'out')
 export const server = path.resolve(__dirname, 'server.js')
